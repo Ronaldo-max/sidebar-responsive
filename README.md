9eb75e777da35776c94585be709f620cc98c9f3b
Ronaldo Costa <ronaldodcs21@gmail.com>
2022-01-29 19:11:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# SIDEBAR RESPONSIVE
+
+## Esse projeto é um menu lateral responsivo, desenvolvido em html, css e javascript um dos desafios do discover.
+
+<br>
+<img src="./sidebar.gif">
+<br>
+
+## Tecnologias
+
+- HTML
+- CSS
+- JAVASCRIPT
+
+## Sites
+
+- Figma
+- Google Fonts
+- Flaticon
+
+## Visualização
+
+- Execute o arquivo principal ( index.html ) no browser.
---
+++ src/script.js
@@ -0,0 +1,62 @@
+const buttonCloseMenu = document.querySelector(".menu-logo-menu-li-button")
+
+let count = 0;
+
+const sidebarContainerTopLogo = document.querySelector(".sidebar-container-top .logo")
+const sidebarMenuListInput = document.querySelector(".sidebar-menu-list input")
+const sidebarMenuListName = document.querySelectorAll(".sidebar-menu-list-name")
+const sidebarContainerBottomPerson = document.querySelector(".sidebar-container-bottom-person")
+
+const sidebar = document.querySelector(".sidebar")
+
+const sidebarContainerTop = document.querySelector(".sidebar-container-top")
+const sidebarMenuList = document.querySelector(".sidebar-menu-list")
+const sidebarContainerBottom = document.querySelector(".sidebar-container-bottom")
+
+const sidebarMenuIcons = document.querySelector(".sidebar-menu-icons")
+
+function closeMenu () {
+    sidebarContainerTopLogo.classList.add("display")
+    sidebarMenuListInput.classList.add("display")
+    sidebarMenuListName.forEach(element => {
+        element.classList.add("display")
+    });
+    sidebarContainerBottomPerson.classList.add("display")
+
+    sidebar.classList.add("new-sidebar")
+
+    sidebarContainerTop.classList.add("menu-icons-new")
+    sidebarMenuList.classList.add("menu-icons-new")
+    sidebarContainerBottom.classList.add("menu-icons-new")
+
+    sidebarMenuIcons.classList.add("new-sidebar-menu-icons")
+
+    count++
+}
+
+function openMenu () {
+    sidebarContainerTopLogo.classList.remove("display")
+    sidebarMenuListInput.classList.remove("display")
+    sidebarMenuListName.forEach(element => {
+        element.classList.remove("display")
+    });
+    sidebarContainerBottomPerson.classList.remove("display")
+
+    sidebar.classList.remove("new-sidebar")
+
+    sidebarContainerTop.classList.remove("menu-icons-new")
+    sidebarMenuList.classList.remove("menu-icons-new")
+    sidebarContainerBottom.classList.remove("menu-icons-new")
+
+    sidebarMenuIcons.classList.remove("new-sidebar-menu-icons")
+
+    count = 0
+}
+
+buttonCloseMenu.addEventListener("click", () => {
+    if(count == 0){
+        closeMenu()
+    } else {
+        openMenu()
+    }
+});
